d18a22c6c0dd50c7e8c2bd095b7eeece8a44ed0f
Francesco Fortini <francifoto98@gmail.com>
2021-02-24 19:44:33 +0100

    commit iniziale

---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard riservata ai clienti impianto scii') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('Benvenuto! Sei loggato come cliente') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
